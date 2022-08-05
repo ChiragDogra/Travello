393dec7c3eb30e24d42485106476ff699d62094b
Chirag Dogra <chirag.dogra79@gmail.com>
2022-08-05 13:41:34 +0530

    database connection issue solved

--- server/index.js
+++ server/index.js
@@ -2,12 +2,15 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 import cors from 'cors';
 
 import postRoutes from './routes/posts.js';
 import userRouter from "./routes/user.js";
+import Connection from './database/db.js';
 
 const app = express();
+dotenv.config();
 
 app.use(express.json({ limit: '30mb', extended: true }))
 app.use(express.urlencoded({ limit: '30mb', extended: true }))
@@ -16,11 +19,13 @@ app.use(cors());
 app.use('/posts', postRoutes);
 app.use("/user", userRouter);
 
-const CONNECTION_URL = 'mongodb+srv://chirag_79:chinuseth@travelo.brxogoe.mongodb.net/?retryWrites=true&w=majority';
-const PORT = process.env.PORT|| 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
-  .catch((error) => console.log(`${error} did not connect`));
+const URL = 'mongodb+srv://chirag_79:chinuseth@travelo.brxogoe.mongodb.net/?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
 
-mongoose.set('useFindAndModify', false);
+app.listen(PORT, () => console.log(`Server is running on PORT: ${PORT}`));
+
+const USERNAME = process.env.DB_USERNAME
+const PASSWORD = process.env.DB_PASSWORD
+
+Connection(USERNAME, PASSWORD);
