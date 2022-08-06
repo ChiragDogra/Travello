cd1638f330a6c1b19df109c42c9d9b0a58ce05e0
Chirag Dogra <chirag.dogra79@gmail.com>
2022-08-06 12:48:23 +0530

    final commit before hosting frontend

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';  // used to make api call
 
-const API = axios.create({ baseURL: 'http://localhost:5000' });
+const API = axios.create({ baseURL: 'https://travellobackend.herokuapp.com/' });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem('profile')) {
